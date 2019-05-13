f420fee1e5d66211aefa5992c3aa2a8f1178872b
Garry Chan <chan6974@gmail.com>
2019-05-13 01:13:34 -0400

    push app

---
+++ app.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from flask import request, Flask, render_template
+
+app = Flask(__name__)
+
+final = pd.read_csv("./final.csv", encoding='utf-8')
+
+sample_list = []
+# home page
+@app.route('/', methods=['GET'])
+def form():
+    sample = final.title_clean.sample()
+    sample_show = sample.values[0].strip().capitalize()
+    sample_list.append([sample_show,sample.index[0]])
+    return render_template("form.html", sample = sample_show)
+
+# Return prediction
+@app.route('/', methods=['POST'])
+def form_post():
+    if request.method == 'POST':
+        guess = request.form['text']
+        sample = sample_list[-1][0] # get the latest sample
+        index = sample_list[-1][1]
+        prediction = final.iloc[index].prediction
+        # ensure probability matches prediction
+        if final.iloc[index].proba_lpt >= 0.5:
+            probability = final.iloc[index].proba_lpt
+        else:
+            probability = 1-final.iloc[index].proba_lpt
+        label = final.iloc[index].label
+    return render_template("result.html", guess = guess, pred = prediction, prob = round(probability*100,2), label = label)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ reddit_scrap.py
@@ -0,0 +1,54 @@
+import datetime
+from time import sleep
+import pandas as pd
+import requests
+
+# https://www.reddit.com/r/ShittyLifeProTips/
+# https://www.reddit.com/r/LifeProTips/
+
+def fetch_page(URL, after=''):
+    '''Returns JSON of top posts of all time'''
+    # URL = f'https://www.reddit.com/r/{subreddit}/top/.json?sort=top&t=all'
+    headers = {'User-agent': 'webscraping bot'}
+    params = {'after': after}
+    r = requests.get(URL, headers=headers, params=params)
+    return r.json()['data']['children']
+
+def parse_post(post):
+    '''Return a post with desired features only'''
+    keep = ['subreddit', 'title', 'score', 'num_comments', 'author', "name"]
+    data = post['data']
+    return {k: v for k, v in data.items() if k in keep}
+
+def parse_page(page):
+    '''Return a tuple of list of posts from a page, and the name of the last post in JSON'''
+    parsed_posts = []
+    after = ''
+    for post in page:
+        p = parse_post(post)
+        after = p['name']
+        parsed_posts.append(p)
+    # after becomes the last post's name, this is way to solve for pagination in the following function
+    # e.g. https://www.reddit.com/r/lifeprotip.json?after=t3_b9073a
+    return parsed_posts, after
+
+def fetch_subreddit(URL, pages=4):
+    '''Return all_posts from subreddit, of the first n pages, each page contains 25 posts'''
+    all_posts = []
+    after = ''
+    for i in range(pages):
+        print(f'Fetching Page {i + 1}')
+        page = fetch_page(URL, after)
+        parsed_posts, after = parse_page(page)
+        all_posts.extend(parsed_posts)
+        sleep(5)
+    return all_posts
+
+if __name__ == "__main__":
+    ''' scrap the following subreddits '''
+
+    all_posts = fetch_subreddit("https://www.reddit.com/r/lifeprotips/top/.json?sort=top&t=all", pages=40)
+    pd.DataFrame(all_posts).to_csv("lpt.csv", index=False)
+
+    all_posts = fetch_subreddit("https://www.reddit.com/r/unethicallifeprotips/top/.json?sort=top&t=all", pages=40)
+    pd.DataFrame(all_posts).to_csv("unlpt.csv",index=False)
